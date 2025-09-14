42c35d37df64f60bc381c9a30df647f83f819327
Marti Gonzalez <martimartigonzalez@gmail.com>
2025-09-14 15:29:36 +0200

    lambdas modifyed

--- Intermediate/03_lambdas.py
+++ Intermediate/03_lambdas.py
@@ -9,7 +9,7 @@ sum_two_values = lambda first_value, second_value: first_value + second_value
 print(sum_two_values(2, 4))
 
 multiply_values = lambda first_value, second_value: first_value * second_value -3
-print(multiply_values(2, 3))
+print(multiply_values(2, 2))
 
 
 #Lambda dentro de una función
